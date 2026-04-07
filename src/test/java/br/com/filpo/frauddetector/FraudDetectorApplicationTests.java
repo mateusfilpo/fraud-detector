543a5c5358011c6ee3041dd5d93fcc275047c1f1
Mateus Filpo <mateusfilpo27@gmail.com>
2026-04-06 21:20:15 -0300

    chore: configuracao inicial do projeto via Spring Initializr

---
+++ src/test/java/br/com/filpo/frauddetector/FraudDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.filpo.frauddetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
